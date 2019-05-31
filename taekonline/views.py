cac82d094d457a2e48f107859b85f6605e147e2b
Fabio Santiago <fabio.ribeiro.santiago@gmail.com>
2019-05-31 09:58:48 -0300

    Initial commit

---
+++ taekonline/views.py
@@ -0,0 +1,109 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login as auth_login, authenticate
+from django.contrib.auth import logout as auth_logout
+from django.contrib import messages
+from django.db.models import Count, F
+from django.utils import timezone
+from django.http import JsonResponse
+
+
+
+def home(request):
+	return render(request, 'home.html')
+
+def profile(request):
+	return render(request, 'profile.html')
+
+def student(request):
+	#students = Student.objects.filter(active=True)
+	#return render(request, 'student.html', {'students':students})
+	return render(request, 'students/student_list.html')
+
+def product(request):
+	return render(request, "products/product_list.html")
+'''
+@login_required
+def car_list(request, template_name='cars/car_list.html'):
+	car = Car.objects.filter(owner=request.user)
+	data = {}
+	data['object_list'] = car
+	return render(request, template_name, data)
+
+@login_required
+def car_view(request, pk, template_name='cars/car_detail.html'):
+	car= get_object_or_404(Car, pk=pk)    
+	return render(request, template_name, {'object':car})
+
+@login_required
+def car_create(request, template_name='cars/car_form.html'):
+	if request.POST:
+		form = CarForm(request.POST)
+		if form.is_valid:
+			obj = form.save(commit=False)
+			obj.owner = request.user
+			obj.save()
+	else:
+		form = CarForm(initial={'owner': request.user})
+	if form.is_valid():
+		form.save()
+		return redirect('car_list')
+	return render(request, template_name, {'form':form})
+
+@login_required
+def car_update(request, pk, template_name='cars/car_form.html'):
+	car= get_object_or_404(Car, pk=pk)
+	form = CarForm(request.POST or None, instance=car)
+	if form.is_valid():
+		form.save()
+		return redirect('car_list')
+	return render(request, template_name, {'form':form})
+
+@login_required
+def car_delete(request, pk, template_name='cars/car_confirm_delete.html'):
+	car= get_object_or_404(Car, pk=pk)    
+	if request.method=='POST':
+		car.delete()
+		return redirect('car_list')
+	return render(request, template_name, {'object':car})
+
+@login_required
+def passenger_join_ride(request, ride_id):
+	ride = Ride.objects.get(id=ride_id)
+	if ride.available_seats > 0:
+		if ride.passengers.filter(id=request.user.id).count() > 0:
+			messages.error(request, 'You are already a passenger of the ride')
+			return redirect('join_ride')
+		passenger = Passenger()
+		passenger.user = request.user
+		passenger.ride = ride
+		passenger.save()
+		#ride.passengers.add(passenger)
+		#ride.save()    
+	return redirect('my_rides_as_passenger')
+
+
+@login_required
+def rating_list(request, template_name='rating/rating_list.html'):
+	drivers_rating = Passenger.objects.filter(user=request.user).filter(driver_rating__isnull=True) 
+	passengers_rating = Passenger.objects.filter(ride__driver=request.user).filter(rating__isnull=True)
+	data = {}
+	data['drivers_rating'] = drivers_rating
+	data['passengers_rating'] = passengers_rating
+	return render(request, template_name, data)
+
+
+def rating_driver(request, passenger_id, rating):
+	passenger = Passenger.objects.get(id=passenger_id)
+	passenger.driver_rating = rating
+	passenger.save()
+	return JsonResponse({'message':'ok'})
+	
+
+
+def rating_passenger(request, passenger_id, rating):
+	passenger = Passenger.objects.get(id=passenger_id)
+	passenger.rating = rating
+	passenger.save()
+	return JsonResponse({'message':'ok'})
+'''
